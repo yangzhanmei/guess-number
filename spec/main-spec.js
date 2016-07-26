5d01786771967f499528d82bb6ac7503eaffd208
fat-gril <15809244435@163.com>
2016-07-26 15:15:44 +0800

    finish compare-number.js and test

---
+++ spec/main-spec.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const CompareNumber = require('../src/compare-number');
+
+describe('CompareNumber', ()=> {
+    const inputs = '1234';
+
+    it('if A and B all are 0', ()=> {
+        const systems = '5678';
+        const expectAnswer = '0A0B';
+        const answer = new CompareNumber(inputs, systems).compareNumber();
+        expect(answer).toEqual(expectAnswer);
+    });
+
+    it('if B is 0', ()=> {
+        const systems = '1234';
+        const answer = new CompareNumber(inputs, systems).compareNumber();
+        const expectAnswer = '4A0B';
+        expect(answer).toEqual(expectAnswer);
+    });
+
+    it('if A is 0', ()=> {
+        const systems = '4321';
+        const expectAnswer = '0A4B';
+        const answer = new CompareNumber(inputs, systems).compareNumber();
+        expect(answer).toEqual(expectAnswer);
+    });
+
+    it('if A and B all are not 0', ()=> {
+        const systems = '4738';
+        const expectAnswer = '1A1B';
+        const answer = new CompareNumber(inputs, systems).compareNumber();
+        expect(answer).toEqual(expectAnswer);
+    })
+});
---
+++ src/compare-number.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class CompareNumber {
+    constructor(inputs, systems) {
+        this.inputs = inputs;
+        this.systems = systems;
+    }
+
+    compareNumber() {
+
+        let inputsArrays = this.inputs.split('');
+        let systemsArrays = this.systems.split('');
+
+        const getBCount = ()=> {
+            return inputsArrays.map((inputsArray, index)=>
+                systemsArrays[index] != inputsArray && systemsArrays.some(systemsArray=>systemsArray === inputsArray) ? 1 : 0
+            ).reduce((a, b)=>a + b);
+        };
+
+        const getACount = ()=> {
+            return inputsArrays.map((inputsArray, index)=>
+                systemsArrays[index] === inputsArray ? 1 : 0
+            ).reduce((a, b)=>a + b);
+        };
+
+        const a = getACount();
+        const b = getBCount();
+
+        return `${a}A${b}B`;
+    }
+}
+
+module.exports = CompareNumber;
